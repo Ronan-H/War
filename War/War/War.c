67d3d670d3da112c4a881819555b0a1e4a49b8ac
Ronan Hanley <g00342393@gmit.ie>
2018-01-02 21:56:17 +0000

    Created some structs and variables

--- War/War/War.c
+++ War/War/War.c
@@ -1,5 +1,40 @@
 #include<stdio.h>;
+#include<string.h>;
+
+struct Card {
+	/*
+		Value: 2-10 -> 2-10 (face value)
+		J, Q, K, A -> 11-14 respectively
+	*/
+	int value;
+	/*
+		Suit: Clubs, Diamonds, Hearts, Spades -> 0-3
+		(actually has no significance in the game)
+	*/
+	int suit;
+};
+
+struct Player {
+	struct Card deck[13];
+	int numRoundsWon;
+};
+
+// used to translate suit numberes to names easily
+const char *  SUIT_NAMES[] = {
+	"Clubs",
+	"Diamonds",
+	"Hearts",
+	"Spades"
+};
 
 void main() {
-	printf("Test");
+	// variables
+	int numPlayers;
+	int roundNum;
+
+	
+
+	printf("War - By Ronan Hanley\n\n");
+	printf("How many players? (2-10):");
+	scanf("%d", numPlayers);
 }
